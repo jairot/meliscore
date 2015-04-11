9a628a1777f69e3d4715d0ede2595faa55a06ba1
Pablo G Celayes <pablocelayes@gmail.com>
2015-04-11 14:57:46 -0300

    * Agregamos score por fotos
    * Agregamos main con ejemplo

--- modelo.py
+++ modelo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from scipy import average
+import requests
 
 URL_BASE = "https://api.mercadolibre.com/"
 
-def score(itemid):
+def generate_scores(itemid):
     """
         Given an itemid, it creates a score
         for the corresponding publication
@@ -24,43 +25,49 @@ def score(itemid):
     partial = {}
 
     # traer item
-    url = URL_BASE + 'items/MLA352546'
+    url = URL_BASE + "items/" + itemid
     res = requests.get(url)
     item_data = res.json()
 
+    n_photos = len(item_data["pictures"])
+    score = min(6, n_photos) * 1.0 / 6
+
+    if n_photos < 2:
+        tip = "Muy pocas fotos!"
+    elif n_photos < 4:
+        tip = "Bien, pero podrías agregar"
+    else:
+        tip = "De diego!"
 
     partial["photo"] = {
-        "score": 0,
-        "tip": "Poné foto culiao!"
+        "score": score,
+        "tip": tip
     }
     
     # traer descripción
-
-
-
-
-
     partial["description"] = {
         "score": 0,
-        "tip": "Poné foto culiao!"
+        "tip": "Media pila che!"
     }
 
     # traer user score
-
-
     partial["user_score"] = {
         "score": 0,
-        "tip": "Poné foto culiao!"
+        "tip": "Media pila che!"
     }
 
 
     # calcular score final
     result = {
-        "total_score": get_total_score(partial_scores),
+        "total_score": get_total_score(partial),
         "partial_scores": partial
     }    
 
     return result
 
 def get_total_score(partial_scores):
-    return average([p["score"] for p in partial_scores])
+    return average([p["score"] for p in partial_scores.values()])
+
+if __name__ == '__main__':
+    scores = generate_scores("MLA554189135")
+    print scores
