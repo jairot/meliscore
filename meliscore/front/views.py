336fbd44fa7383838755988607507c3229f88a4e
jairot <jairotrad@gmail.com>
2015-04-11 15:39:50 -0300

    static root added

--- meliscore/front/views.py
+++ meliscore/front/views.py
@@ -1,6 +1,22 @@
+import re
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+
 def home(request, *args, **kwargs):
     return render_to_response('index.html', locals(),
+                              context_instance=RequestContext(request))
+
+
+def score(request, *args, **kwargs):
+    url = "http://articulo.mercadolibre.com.ar/MLA-548587141-iphone-5s-apple-16gb-retina-tactil-3g-liberado-chip-a6-ios7-_JM"
+    try:
+        item = re.findall(re.findall("MLA-\d+", url))[0]
+    except IndexError:
+        #TODO: retornar al home con un mensaje de error
+        pass
+
+
+    return render_to_response('score.html', locals(),
                               context_instance=RequestContext(request))
