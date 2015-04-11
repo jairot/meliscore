169f6c899dc0443c7aea5e576e0d3839d326a7fe
Pablo G Celayes <pablocelayes@gmail.com>
2015-04-11 17:36:08 -0300

    Agregamos esqueleto de limpieza de json

--- datascience/dataset.py
+++ datascience/dataset.py
@@ -6,10 +6,20 @@ import json
 URL_BASE = "https://api.mercadolibre.com/"
 
 
+def simplify_item(item):
+    """
+        Given an item result from the API
+        it removes all nested information and returns
+        a plain json that is more dataframe-friendly
+    """
+    return item
+
+
 def create_dataset(category_id):
     response = requests.get(URL_BASE + 'sites/MLA/search?category=' + category_id)
-    data = response.json()
-    df = pd.io.json.read_json(json.dumps(data['results']))
+    items = response.json()['results']
+    items = [simplify_item(i) for i in items]
+    df = pd.io.json.read_json(json.dumps(items))
     df.to_csv('iphone5_16gb.csv', encoding='utf-8')
     
 
