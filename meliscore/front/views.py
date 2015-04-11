7d4c512ebf0d652838b33ac8e9395eb4e54ad246
jairot <jairotrad@gmail.com>
2015-04-11 16:29:27 -0300

    Score view

--- meliscore/front/views.py
+++ meliscore/front/views.py
@@ -1,7 +1,10 @@
 import re
+import json
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import JsonResponse
+
 
 from modelo import generate_scores
 
@@ -13,13 +16,12 @@ def home(request, *args, **kwargs):
 def score(request, *args, **kwargs):
     url = "http://articulo.mercadolibre.com.ar/MLA-548587141-iphone-5s-apple-16gb-retina-tactil-3g-liberado-chip-a6-ios7-_JM"
     try:
-        item_id = re.findall(re.findall("MLA-\d+", url))[0]
+        item_id = re.findall("MLA-\d+", url)[0]
     except IndexError:
         #TODO: retornar al home con un mensaje de error
         pass
-
     item_id  = item_id.replace("-", "")
-    generate_scores("item")
-
-    return render_to_response('score.html', locals(),
-                              context_instance=RequestContext(request))
+    score = generate_scores(item_id)
+    return JsonResponse(score)
+    #return render_to_response('score.html', locals(),
+    #                          context_instance=RequestContext(request))
