8b84a5b4d7c53fdd1603a6abbe4ed7974b9355ce
jairot <jairotrad@gmail.com>
2015-04-11 14:54:54 -0300

    Template+url+view

--- meliscore/front/views.py
+++ meliscore/front/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+def home(request, *args, **kwargs):
+    return render_to_response('index.html', locals(),
+                              context_instance=RequestContext(request))
