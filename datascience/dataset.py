13b2b8933e6b68986ee6e74b5021ec611de76f23
mariana <mariana@sammy.workgoup>
2015-04-14 17:32:27 -0300

    paging request

--- datascience/dataset.py
+++ datascience/dataset.py
@@ -7,9 +7,17 @@ URL_BASE = "https://api.mercadolibre.com/"
 
 
 def create_dataset(category_id):
-    response = requests.get(URL_BASE + 'sites/MLA/search?category=' + category_id)
+    response = requests.get(URL_BASE + 'sites/MLA/search?category=' + categoryId)
     data = response.json()
+    limit = data['paging']['limit']
+    offset = data['paging']['offset'] + limit 
+    items_number = data['paging']['total']
     df = pd.io.json.read_json(json.dumps(data['results']))
+    while (offset <= items_number):
+        response = requests.get(URL_BASE + 'sites/MLA/search?category=' + categoryId + '&offset=' + str(offset))
+        data = response.json()
+        df = df.append(pd.io.json.read_json (json.dumps(data['results'])))
+        offset += limit
     df.to_csv('iphone5_16gb.csv', encoding='utf-8')
     
 
